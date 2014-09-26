4d78a42b211456a04a703fe546eded85bd226729
Vladster1256 <vcmariorocks@gmail.com>
2014-09-26 12:29:18 -0600

    Few changes

--- src/chatbot/model/Chatbot.java
+++ src/chatbot/model/Chatbot.java
@@ -6,6 +6,10 @@ public class Chatbot
 	private String name;
 	private int chatCount;
 	
+	/**
+	 * Creates a ChatBot Object with the supplied name and initializes the current number of chats to 0.
+	 * @param name The supplied name for the Chatbot.
+	 */
 	public Chatbot(String name)
 	{
 		this.name = name;
