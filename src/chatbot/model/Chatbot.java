d8aa91af714ec3d38e217ff1b71b1386bfcc7e62
Vladster1256 <vcmariorocks@gmail.com>
2014-09-26 13:23:57 -0600

    Added more descriptions

--- src/chatbot/model/Chatbot.java
+++ src/chatbot/model/Chatbot.java
@@ -7,24 +7,39 @@ package chatbot.model;
  */
 public class Chatbot
 {
-
+	/**
+	 * Creates a ChatBot Object with the supplied name and initializes the current number of chats to 0.
+	 * @param name The supplied name for the Chatbot.
+	 */
 	private String name;
 	private int chatCount;
 	
 	/**
-	 * Creates a ChatBot Object with the supplied name and initializes the current number of chats to 0.
-	 * @param name The supplied name for the Chatbot.
+	 * This makes the Chatbot from this file equal with the chatbot app controller file
+	 * @param name The new name for the Chatbot.
 	 */
 	public Chatbot(String name)
 	{
 		this.name = name;
 	}
 	
+	/**
+	 * This is a really important method. Add lots of lines
+	 * and more
+	 * and more
+	 * more lines
+	 * much wow!!
+	 * @returns name to the chatbot app controller
+	 */
 	public String getName()
 	{
 		return name;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getChatCount()
 	{
 		return chatCount;
