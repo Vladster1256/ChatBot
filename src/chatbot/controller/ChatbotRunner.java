470dcd30964b155a4f1afb2e7dc44c1f1367d8f0
Vladster1256 <vcmariorocks@gmail.com>
2014-10-07 13:40:43 -0600

    More documentation on runner class

--- src/chatbot/controller/ChatbotRunner.java
+++ src/chatbot/controller/ChatbotRunner.java
@@ -1,8 +1,17 @@
 package chatbot.controller;
 
+/**
+ * The runner for chatbot
+ * @author Vladster1256
+ * @version 10/7/14
+ */
 public class ChatbotRunner
 {
-
+	
+	/**
+	 * Starting point for all java programs
+	 * @param args unused!
+	 */
 	public static void main(String[] args)
 	{
 		ChatbotAppController appController = new ChatbotAppController();
